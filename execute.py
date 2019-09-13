64b918a16c5ab953044370c5f5ef63c738f4db57
juan <jpalacio@axletravel.com>
2019-09-13 02:52:53 -0500

    Added args parser to execute file

--- execute.py
+++ execute.py
@@ -10,31 +10,31 @@ ap.add_argument("-b","--build_hdf5")
 args = vars(ap.parse_args())
 
 BASE_PATH = "../input/"
-TRAIN = BASE_PATH + "train.csv"
-TEST = BASE_PATH + "test.csv"
 
 if args["preprocess"]:
+    TRAIN = BASE_PATH + "train.csv"
+    TEST = BASE_PATH + "test.csv"
     write_labels_to_csv(TRAIN,TEST)
     PROCESSED_CSV = "../train_sirna_labels.csv"
     data = pd.read_csv(PROCESSED_CSV)
     dataframe_to_arrray(data)
     free_memory(data)
 
-NUM_TEST_IMAGES = 2700
-NUM_VAL_IMAGES = 2700
-CLEAN_PATH = "../clean_data/train/"
+
 if args["build_hdf5"]:
-################
+    HUVEC_VAL = 4000
+    HEPG2_VAL= 2000
+    RPE_VAL= 2000
+    U2OS_VAL= 800
+    VAL_DISTRIBUTION = [HUVEV_VAL,HEPG2_VAL,RPE_VAL,U2OS_VAL]
+    TEST_DISTRIBUTION = [x/2 for x in VAL_DISTRIBUTION]
+    pick_subset()
+    BUILD_DIMS = 512
+    BUILD_CHANELS = 6
+    CLEAN_PATH = "../clean_data/train/"
+    TRAIN_HDF5 = "../clean_data/hdf5/train.hdf5"
+    VAL_HDF5 = "../clean_data/hdf5/val.hdf5"
+    HDF5_OUTPUTS = [TRAIN_HDF5,VAL_HDF5,TEST_HDF5]
 
-
-#prepare_dataset(RAW_PATH, CLEAN_PATH)
-#check_corrupted_images(CLEAN_PATH)
-splits = split_data(NUM_TEST_IMAGES, NUM_VAL_IMAGES,
-                    CLEAN_PATH)
-
-BUILD_SIZE = 256
-TRAIN_HDF5 = "../clean_data/hdf5/train.hdf5"
-VAL_HDF5 = "../clean_data/hdf5/val.hdf5"
-TEST_HDF5 = "../clean_data/hdf5/test.hdf5"
-HDF5_OUTPUTS = [TRAIN_HDF5,VAL_HDF5,TEST_HDF5]
-write_hdf5(splits, BUILD_SIZE ,HDF5_OUTPUTS)
+    splits = split_data(VAL_DISTRIBUTION,CLEAN_PATH)
+    write_hdf5(splits, BUILD_DIMS,BUILD_CHANELS,HDF5_OUTPUTS)
--- utils/preprocess_utils.py
+++ utils/preprocess_utils.py
@@ -12,60 +12,44 @@ import os
 from glob import glob
 from pathlib import Path
 
-def prepare_dataset(raw_path,clean_path):
-    raw_path = Path(raw_path)
-    print("The data labels are the following:")
-    count = 0
-    for case in raw_path.glob('*'):
-        label = str(case).split("/")[-1]
-        print("Extracting data from ", case)
-        for filename in case.glob("*"):
-            dst = clean_path + label + "-" + str(count)+".png"
-            src =  filename
-            os.rename(src,dst)
-            count+=1
-    print("Extracted a total of",count,"images.")
-
-def check_corrupted_images(clean_path):
-    imagePaths = Path(clean_path)
-    imagePaths = imagePaths.glob('*.png')
-    count = 0
-    for image in imagePaths:
-        im_vector = cv2.imread(str(image))
-        if(im_vector is None):
-            print("BAD IMAGE",image)
-            os.remove(image)
-            continue
-        count +=1
-    print("Total number of good images is",count)
 
 
-def split_data(num_test_images, num_val_images,clean_path):
+def pick_subset(values_distribution, list_paths):
+    HUVEC = values_distribution[0]
+    HEPG2 = values_distribution[1]
+    RPE = values_distribution[2]
+    U2OS = values_distribution[3]
+    for
+
+    return "lool"
+
+def split_data(values_distribution,clean_path):
     np.random.seed(107)
     trainPaths = list(paths.list_images(clean_path))
     np.shuffle(trainPaths)
-    # buff in the format ./dataset/clean_data/data/PNEUMONIA-00.png
-    buff = [x.split("/")[-1] for x in trainPaths]
-    labels = [x.split("-")[0] for x in buff]
-
-    le = LabelEncoder()
-    trainLabels = le.fit_transform(labels)
-
-    test_split = train_test_split(trainPaths, trainLabels,
-	test_size=num_test_images, stratify=trainLabels,
-	random_state=777)
-
-    # perform another stratified sampling, this time to build the
-    # validation data
-    val_split = train_test_split(trainPaths, trainLabels,
-    	test_size=num_val_images, stratify=trainLabels,
-    	random_state=777)
-    return (test_split, val_split)
-
+    subsets = pick_subsets(values_distribution,trainPaths)
+    # # buff in the format ./dataset/clean_data/data/PNEUMONIA-00.png
+    # buff = [x.split("/")[-1] for x in trainPaths]
+    # labels = [x.split("-")[0] for x in buff]
+    #
+    # # le = LabelEncoder()
+    # # trainLabels = le.fit_transform(labels)
+    #
+    # test_split = train_test_split(trainPaths, trainLabels,
+	# test_size=num_test_images, stratify=trainLabels,
+	# random_state=777)
+    #
+    # # perform another stratified sampling, this time to build the
+    # # validation data
+    # val_split = train_test_split(trainPaths, trainLabels,
+    # 	test_size=num_val_images, stratify=trainLabels,
+    # 	random_state=777)
+    # return (test_split, val_split)
+    return "Gucci"
 def write_hdf5(splits,build_size,output_hdf5s):
 
     train_hdf5,val_hdf5,test_hdf5 = output_hdf5s
-    (trainPaths, testPaths, trainLabels, testLabels) = splits[0]
+    (_, testPaths, _, testLabels) = splits[0]
     (trainPaths, valPaths, trainLabels, valLabels) = splits[1]
 
     datasets = [
